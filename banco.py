93db36eab598d657cbc07046ca8d2561130fccef
Guilherme Olegario <guigui300200@gmail.com>
2025-09-06 02:41:43 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -15,14 +15,18 @@ try:
                 print("Seu saldo é de R$2500,00")
             elif escolha.upper() == "NÃO":
                 print("Obrigado por usar nosso banco, volte sempre!")
-            else: 
-                valor = float(input("Qual o valor da transferência? R$"))
-                if valor <= 2500:
-                    print("Transferência realizada com sucesso!")
-                else:
-                    print("Saldo insuficiente para realizar a transferência.")
+            else:
+                print("Opção inválida.")
+
+        elif opcao == "1":
+            valor = float(input("Qual o valor da transferência? R$"))
+            if valor <= 2500:
+                print("Transferência realizada com sucesso!")
+            else:
+                print("Saldo insuficiente para realizar a transferência.")
         else:
             print("Opção inválida, tente novamente.")
+
     else:
         print("Usuário ou senha incorretos. Tente novamente.")       
 
